9f12e23fde1816daa7672c90d3f0f4737beec22a
Michael Chung <mikechung89@gmail.com>
2022-04-04 14:51:13 -0400

    commit tree 9ced489d4d968a46726e9fdeaaea8ba585f56456

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line deli
+    if deli == []
+        puts "The line is currently empty."
+    else 
+        phrase = deli.map.with_index {|each, index| "#{1 + index}. #{each}" }
+        puts "The line is currently: #{phrase.join(' ')}"
+    end
+end
+
+def take_a_number(katz_deli, name)
+    puts "Welcome, #{name}. You are number #{1 + katz_deli.length} in line."
+    katz_deli.push(name)
+end
+
+def now_serving deli
+    if deli.empty? 
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{deli[0]}."
+        deli.shift
+    end
+end
